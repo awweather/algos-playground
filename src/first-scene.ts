978419d26aea8377ea40523475dc7bf7968acf1c
Anthony <awweather@gmail.com>
2023-02-12 22:49:16 -0500

    initial work

--- src/first-scene.ts
+++ src/first-scene.ts
@@ -1,4 +1,101 @@
-import Phaser from 'phaser';
+import Phaser from "phaser";
+
+export class BST {
+  value: number;
+  left: BST | null;
+  right: BST | null;
+  x: number;
+  y: number;
+  circle: Phaser.GameObjects.Arc;
+  scene: Phaser.Scene;
+
+  constructor(value: number, scene: Phaser.Scene, x: number, y: number) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+    this.x = x;
+    this.y = y;
+    this.scene = scene;
+
+    this.circle = scene.add.circle(this.x, this.y, 20, 0xff0000);
+    scene.add.text(this.x, this.y, this.value.toString());
+  }
+
+  insert(
+    value: number,
+    tree: BST | null,
+    startX: number = -1,
+    startY: number = -1
+  ): BST {
+    // Write your code here.
+    // Do not edit the return statement of this method.
+
+    if (!tree) {
+      if (value < this.value)
+        return new BST(
+          value,
+          this.scene,
+          startX > 0 ? startX : this.x - 50,
+          startY > 0 ? startY : this.y + 50
+        );
+      else
+        return new BST(
+          value,
+          this.scene,
+          startX > 0 ? startX : this.x + 50,
+          startY > 0 ? startY : this.y + 50
+        );
+    }
+
+    if (value < tree.value) {
+      tree.left = this.insert(value, tree.left, tree.x - 50, tree.y + 50);
+    } else {
+      tree.right = this.insert(value, tree.right, tree.x + 50, tree.y + 50);
+    }
+
+    return tree;
+  }
+  maxDepth(tree: BST | null): number {
+    if (!tree) {
+      return 0;
+    }
+
+    return Math.max(this.maxDepth(tree.left), this.maxDepth(tree.right)) + 1;
+  }
+  preOrderTraversal() {}
+  postOrderTraversal() {}
+  highlightNode(node: BST) {
+    node.circle.setFillStyle(0x00ff00);
+  }
+
+  unhighlightNode(node: BST) {
+    node.circle.setFillStyle(0xff0000);
+  }
+  async inOrderTraversal(
+    root: BST | null,
+    maxDepth: number,
+    currentDepth: number
+  ) {
+    if (!root) return;
+
+    await this.inOrderTraversal(root.left, maxDepth, currentDepth + 1);
+    root.circle.setFillStyle(0xffff00, 1);
+    await new Promise((resolve) => setTimeout(resolve, 500));
+    root.circle.setFillStyle(0xff0000, 1);
+    await this.inOrderTraversal(root.right, maxDepth, currentDepth + 1);
+  }
+
+  contains(value: number) {
+    // Write your code here.
+    return false;
+  }
+
+  remove(value: number): BST {
+    // Write your code here.
+    // Do not edit the return statement of this method.
+    return this;
+  }
+}
 
 /**
  * FirstGameScene is an example Phaser Scene
@@ -7,165 +104,121 @@ import Phaser from 'phaser';
  * @public
  */
 export class FirstGameScene extends Phaser.Scene {
+  constructor() {
+    super("FirstGameScene");
+    console.log("FirstGameScene.constructor()");
+  }
 
-    private gameOver!: boolean;
-    private score!: number;
-    private platforms!: Phaser.Physics.Arcade.StaticGroup;
-    private player!: Phaser.Physics.Arcade.Sprite;
-    private cursors!: Phaser.Types.Input.Keyboard.CursorKeys;
-    private stars!: Phaser.Physics.Arcade.Group;
-    private bombs!: Phaser.Physics.Arcade.Group;
-    private scoreText!: Phaser.GameObjects.Text;
+  preload() {
+    console.log("FirstGameScene.preload");
+    this.load.image("sky", "assets/sky.png");
+    this.load.image("ground", "assets/platform.png");
+    this.load.image("star", "assets/star.png");
+    this.load.image("bomb", "assets/bomb.png");
+    this.load.spritesheet("dude", "assets/dude.png", {
+      frameWidth: 32,
+      frameHeight: 48,
+    });
+  }
 
-    constructor() {
-        super('FirstGameScene');
-        console.log('FirstGameScene.constructor()');
+  create() {
+    // Create the initial tree using the BST class
+    const tree = new BST(50, this, 300, 300);
+    tree.insert(20, tree);
+    tree.insert(10, tree);
+    tree.insert(30, tree);
+    tree.insert(40, tree);
+    tree.insert(60, tree);
+    tree.insert(70, tree);
+    tree.insert(80, tree);
+
+    const maxDepth = tree.maxDepth(tree);
+    tree.inOrderTraversal(tree, maxDepth, 0);
+    // Create the graphical representation of the tree
+
+    // this.createGraphicsForTree(tree, 0, 25, 300, 300);
+  }
+
+  createGraphicsForTree(
+    node: BST | null,
+    depth: number,
+    nodeDistance: number,
+    startX: number,
+    startY: number
+  ): void {
+    if (!node) {
+      return;
     }
 
-    preload() {
-        console.log('FirstGameScene.preload');
-        this.load.image('sky', 'assets/sky.png');
-        this.load.image('ground', 'assets/platform.png');
-        this.load.image('star', 'assets/star.png');
-        this.load.image('bomb', 'assets/bomb.png');
-        this.load.spritesheet('dude', 'assets/dude.png', { frameWidth: 32, frameHeight: 48 });
+    this.add.circle(startX, startY, 20, 0xff0000);
+    this.add.text(startX, startY, node.value.toString());
+
+    depth++;
+
+    this.createGraphicsForTree(
+      node.left,
+      depth,
+      nodeDistance,
+      startX - 50,
+      startY + 50
+    );
+    this.createGraphicsForTree(
+      node.right,
+      depth + 1,
+      nodeDistance,
+      startX + 50,
+      startY + 50
+    );
+  }
+
+  // updateGraphicsForTree(tree: BST, graphics: any) {
+  //   // Clear existing graphics
+  //   graphics.clear();
+
+  //   // Recursively draw nodes for the tree
+  //   function drawNode(node: BST, x: number, y: number) {
+  //     if (!node) return;
+
+  //     // Draw a circle for the node
+  //     graphics.fillCircle(x, y, nodeRadius);
+
+  //     // Draw lines to children nodes
+  //     if (node.left) {
+  //       graphics.lineStyle(lineThickness, lineColor);
+  //       graphics.moveTo(x, y);
+  //       graphics.lineTo(x - nodeDistance, y + nodeDistance);
+  //       drawNode(node.left, x - nodeDistance, y + nodeDistance);
+  //     }
+  //     if (node.right) {
+  //       graphics.lineStyle(lineThickness, lineColor);
+  //       graphics.moveTo(x, y);
+  //       graphics.lineTo(x + nodeDistance, y + nodeDistance);
+  //       drawNode(node.right, x + nodeDistance, y + nodeDistance);
+  //     }
+  //   }
+
+  //   drawNode(tree, treeX, treeY);
+  // }
+  highlightNode(
+    tree: BST | null,
+    value: number,
+    graphics: Phaser.GameObjects.Graphics
+  ) {
+    if (!tree) {
+      return;
     }
 
-    create() {
-        console.log('FirstGameScene.create');
-        // initialize variables
-        this.gameOver = false;
-        this.score = 0;
-
-        //  A simple background for our game
-        this.add.image(400, 300, 'sky');
-
-        //  The platforms group contains the ground and the 2 ledges we can jump on
-        this.platforms = this.physics.add.staticGroup();
-
-        //  Here we create the ground.
-        //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
-        this.platforms.create(400, 568, 'ground').setScale(2).refreshBody();
-
-        //  Now let's create some ledges
-        this.platforms.create(600, 400, 'ground');
-        this.platforms.create(50, 250, 'ground');
-        this.platforms.create(750, 220, 'ground');
-
-        // The player and its settings
-        this.player = this.physics.add.sprite(100, 450, 'dude');
-
-        //  Player physics properties. Give the little guy a slight bounce.
-        this.player.setBounce(0.2);
-        this.player.setCollideWorldBounds(true);
-
-        //  Our player animations, turning, walking left and walking right.
-        this.anims.create({
-            key: 'left',
-            frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
-            frameRate: 10,
-            repeat: -1
-        });
-
-        this.anims.create({
-            key: 'turn',
-            frames: [{ key: 'dude', frame: 4 }],
-            frameRate: 20
-        });
-
-        this.anims.create({
-            key: 'right',
-            frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
-            frameRate: 10,
-            repeat: -1
-        });
-
-        //  Input Events
-        this.cursors = this.input.keyboard.createCursorKeys();
-
-        //  Some stars to collect, 12 in total, evenly spaced 70 pixels apart along the x axis
-        this.stars = this.physics.add.group({
-            key: 'star',
-            repeat: 11,
-            setXY: { x: 12, y: 0, stepX: 70 }
-        });
-
-        this.stars.children.iterate((child: Phaser.GameObjects.GameObject) => {
-            //  Give each star a slightly different bounce
-            (child as Phaser.Physics.Arcade.Sprite).setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
-        });
-
-        this.bombs = this.physics.add.group();
-
-        //  The score
-        this.scoreText = this.add.text(16, 16, 'score: 0');
-        this.scoreText.style.fontSize = '32px';
-        this.scoreText.style.setFill('#000');
-
-        //  Collide the player and the stars with the platforms
-        this.physics.add.collider(this.player, this.platforms);
-        this.physics.add.collider(this.stars, this.platforms);
-        this.physics.add.collider(this.bombs, this.platforms);
-
-        //  Checks to see if the player overlaps with any of the stars, if he does call the collectStar function
-        this.physics.add.overlap(this.player, this.stars, this.collectStar, undefined, this);
-
-        this.physics.add.collider(this.player, this.bombs, this.hitBomb, undefined, this);
+    if (tree.value === value) {
+      graphics.fillStyle(0xff0000, 1);
+    } else {
+      graphics.fillStyle(0x000000, 1);
     }
 
-    update() {
-        if (this.gameOver) {
-            return;
-        }
+    graphics.fillCircle(tree.x, tree.y, 10);
+    // graphics.fillText(tree.value.toString(), tree.x, tree.y);
 
-        if (this.cursors.left.isDown) {
-            this.player.setVelocityX(-160);
-
-            this.player.anims.play('left', true);
-        }
-        else if (this.cursors.right.isDown) {
-            this.player.setVelocityX(160);
-
-            this.player.anims.play('right', true);
-        }
-        else {
-            this.player.setVelocityX(0);
-
-            this.player.anims.play('turn');
-        }
-
-        if (this.cursors.up.isDown && this.player.body.touching.down) {
-            this.player.setVelocityY(-330);
-        }
-    }
-
-    collectStar(playerGO: Phaser.GameObjects.GameObject, starGO: Phaser.GameObjects.GameObject) {
-        const player = playerGO as Phaser.Physics.Arcade.Sprite;
-        const star = starGO as Phaser.Physics.Arcade.Sprite;
-        star.disableBody(true, true);
-        //  Add and update the score
-        this.score += 10;
-        this.scoreText.setText('Score: ' + this.score);
-        if (this.stars.countActive(true) === 0) {
-            //  A new batch of stars to collect
-            this.stars.children.iterate((childGO: Phaser.GameObjects.GameObject) => {
-                const child = childGO as Phaser.Physics.Arcade.Sprite;
-                child.enableBody(true, child.x, 0, true, true);
-            });
-            const x = (player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
-            const bomb = this.bombs.create(x, 16, 'bomb');
-            bomb.setBounce(1);
-            bomb.setCollideWorldBounds(true);
-            bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
-            bomb.allowGravity = false;
-        }
-    }
-
-    hitBomb(playerGO: Phaser.GameObjects.GameObject) {
-        const player = playerGO as Phaser.Physics.Arcade.Sprite;
-        this.physics.pause();
-        player.setTint(0xff0000);
-        player.anims.play('turn');
-        this.gameOver = true;
-    }
+    this.highlightNode(tree.left, value, graphics);
+    this.highlightNode(tree.right, value, graphics);
+  }
+  update() {}
 }
